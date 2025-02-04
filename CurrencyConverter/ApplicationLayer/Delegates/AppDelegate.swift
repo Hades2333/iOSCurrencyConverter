d1960853b47bbbaf33dc9e29b74f35bd544ea30a
Mikalai Bekliamishchau <mikalai.bekliamishchau.iosdev@gmail.com>
2025-02-04 12:29:44 +0100

    configure delegates

--- CurrencyConverter/ApplicationLayer/Delegates/AppDelegate.swift
+++ CurrencyConverter/ApplicationLayer/Delegates/AppDelegate.swift
@@ -7,30 +7,26 @@
 
 import UIKit
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
 
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        let client: HTTPClient = HTTPClient()
+        let repository: ExchangeRateRepository = .init(client: client)
+        let excahngeUseCase = GetExchangeRateUseCase(repository: repository)
+        let DefaultsService: DefaultsServiceProtocol = DefaultsService()
+        let viewModel = ExchangeRateViewModel(getExchangeRateUseCase: excahngeUseCase, defaultsService: DefaultsService)
+        let rootViewController = ExchangeRateViewController(viewModel: viewModel)
 
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = rootViewController
+        window?.makeKeyAndVisible()
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
 }
-
